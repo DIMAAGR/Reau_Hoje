c9af8f37087f2cd5e3bc8fb69a67ac6a55159a33
Italo Matos <attackdiamond@gmail.com>
2021-04-09 16:10:50 -0300

    adicionado formulas e conexções com os contratos

--- lib/views/mainScreen.dart
+++ lib/views/mainScreen.dart
@@ -11,18 +11,27 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   var url = "https://bsc-dataseed1.binance.org:443";
+
   var httpClient = new Client();
+
   Web3Client web3;
-  final myAdress = MyPreferences.getWallet();
+  final myAdress = "0x056982E47890Ff8eeac1d57bb36Ef48Dd0D5A823";
   int amount = 0;
-  var myData;
+
+  //Definições Obtidas a partir do contrato do reau
+  var totalFees;
+  var walletValue;
+  var deadBalance;
+  double reauBNBprice;
+  double reauUSDPrice;
+  double dollarPrice;
   bool data;
 
   @override
   void initState() {
     web3 = Web3Client(url, httpClient);
     debugPrint(myAdress);
-    getbalance(MyPreferences.getWallet());
+    startReauOptions(myAdress);
     super.initState();
   }
 
@@ -77,9 +86,9 @@ class _MainScreenState extends State<MainScreen> {
                   Expanded(
                     flex: 2,
                     child: Center(
-                      child: myData != null
+                      child: walletValue != null
                           ? Text(
-                              myData.toString(),
+                              walletValue.toString(),
                               style: TextStyle(
                                   fontSize: 55,
                                   color: Color.fromARGB(255, 248, 248, 248),
@@ -133,14 +142,9 @@ class _MainScreenState extends State<MainScreen> {
 
 //==============================================================================================//
   // CARREGA O CONTRATO DO VIRA LATA FINANCE REAU
-  Future<DeployedContract> loadReauContract() async {
-    String abi =
-        await rootBundle.loadString('lib/assets/abi.json'); //recebe o JSON ABI
-    String contractAdress =
-        '0x4c79b8c9cB0BD62B047880603a9DEcf36dE28344'; //gera o contrato a partir do reauContract
-
-    final contract = DeployedContract(
-        ContractAbi.fromJson(abi, 'ViraLataFinance'),
+  Future<DeployedContract> loadContract(
+      String contractAdress, String abi, String contractName) async {
+    final contract = DeployedContract(ContractAbi.fromJson(abi, contractName),
         EthereumAddress.fromHex(contractAdress));
     return contract;
   }
@@ -148,9 +152,10 @@ class _MainScreenState extends State<MainScreen> {
 //==============================================================================================//
   // Tem a função de transferir os argumentos carregar o contrato
   // e receber a informação desejada do servidor
-  Future<List<dynamic>> reauQuery(
-      String functionName, List<dynamic> args) async {
-    final contract = await loadReauContract();
+  Future<List<dynamic>> query(
+      {String functionName,
+      List<dynamic> args,
+      DeployedContract contract}) async {
     final ethFunction = contract.function(functionName);
     final result = await web3.call(
         contract: contract, function: ethFunction, params: args);
@@ -160,17 +165,100 @@ class _MainScreenState extends State<MainScreen> {
 
 //==============================================================================================//
   // RECEBE O VALOR NUMERICO(A QUANTIDADE) de REAUS QUE VOCÊ TEM EM SUA CARTEIRA
-  Future<void> getbalance(String wallet) async {
-    // Esta Função faz a conversão da carteira para HEX
-    //  EthereumAddress address = EthereumAddress.fromHex(wallet);
+  Future<void> startReauOptions(String wallet) async {
+    // INICIA ABI
+    String abi = await rootBundle.loadString('lib/assets/abi.json');
+
+    //Carrega o Contrato do ViraLataFinance recebendo o Contrato o tipo ABI e o Nome do Contrato
+    final contract =
+        await loadContract(ProgramData().reauContract, abi, "ViraLataFinance");
 
     //Mostra o Balanço que há em sua carteira!
-    List<dynamic> result =
-        await reauQuery("balanceOf", [EthereumAddress.fromHex(myAdress)]);
-    myData = result[0];
+    List<dynamic> walletBalanceResult = await query(
+        functionName: "balanceOf",
+        args: [EthereumAddress.fromHex(myAdress)],
+        contract: contract);
+
+    // Mostra o totalSupply do Vira Lata Finance Reau
+    List<dynamic> totalFeesResult =
+        await query(functionName: "totalFees", args: [], contract: contract);
+
+    // Mostra o total de moedas mortas
+    List<dynamic> deadBalanceResult = await query(
+        functionName: "balanceOf",
+        args: [
+          EthereumAddress.fromHex("0x000000000000000000000000000000000000dead")
+        ],
+        contract: contract);
+
+    //Retorno das Informações
+    walletValue = walletBalanceResult[0];
+    walletValue = walletBalanceResult[0];
+    deadBalance = deadBalanceResult[0];
+    totalFees = totalFeesResult[0];
     data = true;
     setState(() {});
+    loadPancake();
   }
-//==============================================================================================//
 
+//==============================================================================================//
+//
+//Carrega o contrato da pancake
+  Future<void> loadPancake() async {
+    String abi = await rootBundle.loadString('lib/assets/lpAbi.json');
+    final contract = await loadContract(
+        "0x7Cc956136C36e7Fbd6B74C07d9E40Eccd3779249",
+        abi,
+        "PancakePair"); // Carregará o pancake contract
+
+    List<dynamic> getReauReserves =
+        await query(functionName: "getReserves", args: [], contract: contract);
+    var reauPriceBnb = (getReauReserves[1] / getReauReserves[0]) * 0.000000001;
+    reauBNBprice = reauPriceBnb;
+    print("reauBNBprice: " + reauPriceBnb.toString());
+    loadBnbUsd();
+  }
+
+//Carrega o contrato da BNBUSD
+  Future<void> loadBnbUsd() async {
+    // contract: 0x1B96B92314C44b159149f7E0303511fB2Fc4774f
+    //
+    String abi = await rootBundle.loadString('lib/assets/lpAbi.json');
+    final contract = await loadContract(
+        "0x1B96B92314C44b159149f7E0303511fB2Fc4774f",
+        abi,
+        "PancakePair"); // Carregará o pancake contract
+    //Receberá o BNBUSD Reserves
+    List<dynamic> getBnbUsdReserves =
+        await query(functionName: "getReserves", args: [], contract: contract);
+    //Calculará o preço do bnb;
+    double bnbPrice = getBnbUsdReserves[1] / getBnbUsdReserves[0];
+    debugPrint("BnbPrice: " + bnbPrice.toString());
+    double a = bnbPrice;
+    double b = reauBNBprice;
+    reauUSDPrice = a * b;
+    debugPrint("ReauPrice: " + reauUSDPrice.toString());
+    loadCBrl();
+  }
+
+  Future<void> loadCBrl() async {
+    // contract: 0x1B96B92314C44b159149f7E0303511fB2Fc4774f
+    //
+    String abi = await rootBundle.loadString('lib/assets/lpAbi.json');
+    final contract = await loadContract(
+        "0x3E820DF7D7086DE2D46d908d04c0A24968B32b94",
+        abi,
+        "PancakePair"); // Carregará o pancake contract
+    //Receberá o BNBUSD Reserves
+    List<dynamic> getCbrlReserves =
+        await query(functionName: "getReserves", args: [], contract: contract);
+    //Calculará o preço do bnb;
+    double cBrlprice =
+        (getCbrlReserves[0] / getCbrlReserves[1]) * 1000000000000;
+    debugPrint("cBRLPrice: " + cBrlprice.toString());
+    double a = cBrlprice;
+    double b = reauBNBprice;
+    dollarPrice = a * b;
+    debugPrint("dollarPrice: " + dollarPrice.toString());
+  }
 }
