41d6a9213160ffccc9f27e73cb7614c3a8047e97
Italo Matos <attackdiamond@gmail.com>
2021-04-12 14:14:31 -0300

    movido widget

--- lib/views/main_screen/components/difference.dart
+++ lib/views/main_screen/components/difference.dart
@@ -1,8 +1,84 @@
 import 'package:flutter/material.dart';
 
 class Difference extends StatelessWidget {
+  final double difference;
+  final double ancientdifference;
+  final String diffstring;
+
+  Difference({this.difference, this.ancientdifference, this.diffstring});
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return difference > ancientdifference
+        ? Padding(
+            padding: const EdgeInsets.only(right: 16.0, top: 12.0),
+            child: Container(
+              height: 28,
+              width: 58 + diffstring.length.toDouble() * 2.4,
+              decoration: BoxDecoration(
+                  color: Colors.greenAccent[700],
+                  borderRadius: BorderRadius.circular(5)),
+              child: Center(
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.keyboard_arrow_up,
+                      color: Colors.white,
+                    ),
+                    Text(
+                      diffstring,
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w900),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        : difference < ancientdifference
+            ? Padding(
+                padding: const EdgeInsets.only(right: 16.0, top: 12.0),
+                child: Container(
+                  height: 28,
+                  width: 64 + diffstring.length.toDouble() * 2.3,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent[700],
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Center(
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.keyboard_arrow_down,
+                          color: Colors.white,
+                        ),
+                        Text(
+                          diffstring,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w900),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            : difference == 0.0
+                ? Padding(
+                    padding: const EdgeInsets.only(right: 16.0, top: 12.0),
+                    child: Container(
+                      height: 28,
+                      width: 58,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[700],
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Center(
+                        child: Text(
+                          diffstring,
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w900),
+                        ),
+                      ),
+                    ),
+                  )
+                : SizedBox();
   }
 }
--- lib/views/main_screen/mainScreen.dart
+++ lib/views/main_screen/mainScreen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart';
 import 'package:reau_hoje/data/data.dart';
+import 'package:reau_hoje/views/main_screen/components/difference.dart';
 import 'package:web3dart/web3dart.dart';
 
 class MainScreen extends StatefulWidget {
@@ -110,7 +111,11 @@ class _MainScreenState extends State<MainScreen> {
                     ),
                   ),
                 ),
-                _reauVariation(diffstring),
+                Difference(
+                  ancientdifference: ancientdifference,
+                  difference: difference,
+                  diffstring: diffstring,
+                )
               ],
             ),
             SizedBox(
@@ -174,80 +179,6 @@ class _MainScreenState extends State<MainScreen> {
         ),
       ),
     );
-  }
-
-  _reauVariation(String diffstring) {
-    return difference > ancientdifference
-        ? Padding(
-            padding: const EdgeInsets.only(right: 16.0, top: 12.0),
-            child: Container(
-              height: 28,
-              width: 58 + diffstring.length.toDouble() * 2.4,
-              decoration: BoxDecoration(
-                  color: Colors.greenAccent[700],
-                  borderRadius: BorderRadius.circular(5)),
-              child: Center(
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.keyboard_arrow_up,
-                      color: Colors.white,
-                    ),
-                    Text(
-                      diffstring,
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.w900),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          )
-        : difference < ancientdifference
-            ? Padding(
-                padding: const EdgeInsets.only(right: 16.0, top: 12.0),
-                child: Container(
-                  height: 28,
-                  width: 64 + diffstring.length.toDouble() * 2.3,
-                  decoration: BoxDecoration(
-                      color: Colors.redAccent[700],
-                      borderRadius: BorderRadius.circular(5)),
-                  child: Center(
-                    child: Row(
-                      children: [
-                        Icon(
-                          Icons.keyboard_arrow_down,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          diffstring,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w900),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            : difference == 0.0
-                ? Padding(
-                    padding: const EdgeInsets.only(right: 16.0, top: 12.0),
-                    child: Container(
-                      height: 28,
-                      width: 58,
-                      decoration: BoxDecoration(
-                          color: Colors.grey[700],
-                          borderRadius: BorderRadius.circular(5)),
-                      child: Center(
-                        child: Text(
-                          diffstring,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.w900),
-                        ),
-                      ),
-                    ),
-                  )
-                : SizedBox();
   }
 
   Future<void> startTimer() async {
