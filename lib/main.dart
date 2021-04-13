44a062b06e7d8370ca6deeb66da034a0410add58
Italo Matos <attackdiamond@gmail.com>
2021-04-13 12:53:34 -0300

    refatorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:reau_hoje/data/data.dart';
+import 'package:reau_hoje/providers/reauprovider.dart';
 import 'package:reau_hoje/routers/application_routers.dart';
 import 'package:reau_hoje/views/hello.dart';
 import 'package:reau_hoje/views/main_screen/main_screen.dart';
@@ -18,20 +20,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //  SharedPreferences.setMockInitialValues({});
-    return MaterialApp(
-      title: 'ReAU Hoje',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ReauProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Reau Hoje',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
 
-      // home: MyPreferences.getWallet().isEmpty ? FirstTake() : Starting(),
-      routes: {
-        AppRoutes.HOME: (ctx) => MainScreen(),
-        AppRoutes.STARTING: (ctx) => Starting(),
-        AppRoutes.HELLO: (ctx) => FirstTake(),
-        AppRoutes.MYWALLET: (ctx) => MyWallet(),
-        AppRoutes.MAINSCREEN: (ctx) => MainScreen(),
-      },
+        // home: MyPreferences.getWallet().isEmpty ? FirstTake() : Starting(),
+        routes: {
+          AppRoutes.HOME: (ctx) => MainScreen(),
+          AppRoutes.STARTING: (ctx) => Starting(),
+          AppRoutes.HELLO: (ctx) => FirstTake(),
+          AppRoutes.MYWALLET: (ctx) => MyWallet(),
+          AppRoutes.MAINSCREEN: (ctx) => MainScreen(),
+        },
+      ),
     );
   }
 }
