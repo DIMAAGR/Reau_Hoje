4a9ac5eb009ecd54728610f28f467641fa5a4e15
Italo Matos <attackdiamond@gmail.com>
2021-04-15 00:16:37 -0300

    modificado para a adição de troca de moedas

--- lib/views/main_screen/components/custom_reau_app_bar.dart
+++ lib/views/main_screen/components/custom_reau_app_bar.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:reau_hoje/data/reauconnection.dart';
 import 'package:reau_hoje/views/main_screen/components/difference.dart';
 
-class CustomReauAppBar extends StatelessWidget {
+class CustomReauAppBar extends StatefulWidget {
   const CustomReauAppBar({
     Key key,
+    @required this.rc,
     @required this.appUser,
     @required this.updown,
     @required this.difference,
@@ -12,11 +14,18 @@ class CustomReauAppBar extends StatelessWidget {
 
   final String appUser;
   final bool updown;
+  final ReauConnection rc;
   final double difference;
   final String diffstring;
 
   @override
+  _CustomReauAppBarState createState() => _CustomReauAppBarState();
+}
+
+class _CustomReauAppBarState extends State<CustomReauAppBar> {
+  @override
   Widget build(BuildContext context) {
+    String moneyType = widget.rc.getCurrentType();
     return Padding(
       padding: const EdgeInsets.only(top: 24.0),
       child: Row(
@@ -27,26 +36,39 @@ class CustomReauAppBar extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.only(right: 12.0, top: 12.0),
-            child: Container(
-              height: 28,
-              width: 46,
-              decoration: BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.circular(5)),
-              child: Center(
-                child: Text(
-                  "BRL",
-                  style: TextStyle(
-                      color: Color.fromARGB(255, 248, 248, 248),
-                      fontWeight: FontWeight.w900),
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  moneyType == "BRL" ? moneyType = "USD" : moneyType = "BRL";
+                  moneyType == "BRL"
+                      ? widget.rc.defCurrentType("BRL")
+                      : widget.rc.defCurrentType("USD");
+
+                  widget.rc.setCurrency();
+                });
+              },
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                height: 28,
+                width: 46,
+                decoration: BoxDecoration(
+                    color: Colors.black45,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Center(
+                  child: Text(
+                    moneyType,
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 248, 248, 248),
+                        fontWeight: FontWeight.w900),
+                  ),
                 ),
               ),
             ),
           ),
           Difference(
-            updown: updown == null ? true : updown,
-            difference: difference,
-            diffstring: diffstring,
+            updown: widget.updown == null ? true : widget.updown,
+            difference: widget.difference,
+            diffstring: widget.diffstring,
           )
         ],
       ),
