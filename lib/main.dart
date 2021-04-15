ea208eed64a383ebba25fdc087b6b2893a29d2c7
Italo Matos <attackdiamond@gmail.com>
2021-04-14 21:06:29 -0300

    refatorado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:reau_hoje/data/data.dart';
-
 import 'package:reau_hoje/routers/application_routers.dart';
 import 'package:reau_hoje/views/hello.dart';
 import 'package:reau_hoje/views/main_screen/main_screen.dart';
@@ -9,6 +8,7 @@ import 'package:reau_hoje/views/myWallet.dart';
 import 'package:reau_hoje/views/starting.dart';
 
 Future main() async {
+  print("hello!");
   WidgetsFlutterBinding.ensureInitialized(); //Incializador Binding
   await MyPreferences.init(); // Inicializa o SharedPreferences
   runApp(MyApp());
