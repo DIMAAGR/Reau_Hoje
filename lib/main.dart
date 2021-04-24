57462174c83bc9f218abcf24e7903b5e6f739e59
Italo Matos <attackdiamond@gmail.com>
2021-04-23 23:51:32 -0300

    removido o provedor

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:reau_hoje/data/data.dart';
 import 'package:reau_hoje/routers/application_routers.dart';
+import 'package:reau_hoje/views/Calculator/calculator_screen.dart';
 import 'package:reau_hoje/views/hello.dart';
-import 'package:reau_hoje/views/main_screen/main_screen.dart';
+import 'package:reau_hoje/views/main_screen/body/main_screen.dart';
 import 'package:reau_hoje/views/myWallet.dart';
 import 'package:reau_hoje/views/starting.dart';
 
@@ -32,6 +34,7 @@ class MyApp extends StatelessWidget {
         AppRoutes.HELLO: (ctx) => FirstTake(),
         AppRoutes.MYWALLET: (ctx) => MyWallet(),
         AppRoutes.MAINSCREEN: (ctx) => MainScreen(),
+        AppRoutes.CALCULATOR: (ctx) => Calculator(),
       },
     );
   }
